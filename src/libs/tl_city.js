3a8d408f9f4041ae14d6782b2acfdc26532b9012
StephaneBranly <stephanebranly.pro@gmail.com>
2019-10-30 23:56:00 +0100

    Fx mise en async pour download (return a revoir)

--- src/libs/tl_city.js
+++ src/libs/tl_city.js
@@ -1,7 +1,12 @@
-export function getCityTLFromId (text) {
-    const url = 'https://www.instagram.com/explore/locations/'+text+'/?__a=1'
+export async function getCityTLFromId (text) {
+    const url = 'https://www.instagram.com/explore/locations/'+text+'/?__a=1';
     console.log(url);
-    return fetch(url)
-      .then((response) => response.json())
-      .catch((error) => console.error(error))
+    try {
+    const response = await fetch(url);
+    console.log(await response.json());
+    return await response.json();
+  }
+  catch (error) {
+    return console.error(error);
+  }
 }
